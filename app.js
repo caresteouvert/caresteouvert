750ce396b7f84f21dee4a6f929637aec2fdcfff9
François de Metz <francois@2metz.fr>
2020-03-23 16:08:37 +0100

    Fix 404.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('./dist'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port);
