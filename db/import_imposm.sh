b55c8d37bbd154b445867ed409730b06fbaeba9b
vdct <vdct@laposte.net>
2020-03-26 17:08:27 +0100

    +Fichier de conf osmosis (#22)

--- db/import_imposm.sh
+++ db/import_imposm.sh
@@ -12,5 +12,10 @@ wget -NS http://download.openstreetmap.fr/extracts/merge/${DATASET}.state.txt
 mkdir -p /data/download/diffs
 mv ${DATASET}.state.txt /data/download/diffs/state.txt
 
+
 imposm import -mapping /git/covid19_map/db/import.yml -read /data/download/${DATASET}.osm.pbf -overwritecache -cachedir /data/imposm_cache
 imposm import -write -connection ${CONNEXION}?prefix=NONE -mapping /git/covid19_map/db/import.yml -cachedir /data/imposm_cache -dbschema-import public -diff
+
+
+echo baseUrl=http://download.openstreetmap.fr/replication/merge/${DATASET}/minute/ > /data/download/diffs/configuration.txt
+echo maxInterval = 7200 >> /data/download/diffs/configuration.txt 
