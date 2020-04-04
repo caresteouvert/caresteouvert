72a4d0c65c4fcf6d1da62f82a483951781e59f02
François de Metz <francois@2metz.fr>
2020-04-04 18:29:01 +0200

    Fix a duplicate navigation error when opening the POI sidebar.

--- js/map.vue
+++ js/map.vue
@@ -261,10 +261,14 @@ export default {
     },
 
     clickPoi(e) {
+      const id = e.mapboxEvent.features[0].properties.fid;
+      if (this.$route.name === 'place' && this.$route.params.id === id) {
+        return;
+      }
       this.$router.push({
         name: 'place',
         params: {
-          id: e.mapboxEvent.features[0].properties.fid,
+          id,
           featuresAndLocation: this.featuresAndLocation
         }
       });
