bfb19ac0b0c1224e14ded628eb779d2d0da94e2f
PanierAvide <panieravide@riseup.net>
2020-04-07 14:06:38 +0200

    Force name of geojson countries table

--- db/load_countries.sh
+++ db/load_countries.sh
@@ -3,4 +3,4 @@
 GIT_ROOT=$(dirname $(dirname $(realpath ${0})))
 CONNEXION_STRING=${1}
 
-docker run --network=host -v ${GIT_ROOT}:/git/covid19_map -ti osgeo/gdal:alpine-small-latest ogr2ogr -f "PostgreSQL" PG:"${CONNEXION_STRING}" -s_srs EPSG:4326 -t_srs EPSG:3857 -overwrite -skipfailures /git/covid19_map/db/countries_subcountries.geojson
+docker run --network=host -v ${GIT_ROOT}:/git/covid19_map -ti osgeo/gdal:alpine-small-latest ogr2ogr -f "PostgreSQL" PG:"${CONNEXION_STRING}" -s_srs EPSG:4326 -t_srs EPSG:3857 -overwrite -nln countries_subcountries -skipfailures /git/covid19_map/db/countries_subcountries.geojson
