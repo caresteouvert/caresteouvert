ad5ad5f22aa5b5a866ffe78f28e0520273d9c689
vdct <vdct@laposte.net>
2020-04-09 20:49:48 +0200

    Subdivide layer for perf - Update with tmp layer and rename

---
+++ db/post_load_countries.sql
@@ -0,0 +1,17 @@
+WITH
+complex_areas_to_subdivide
+AS (
+DELETE FROM tmp_countries_subcountries
+WHERE ST_NPoints(wkb_geometry) > 255
+returning "name",country_iso2,sub_country,wkb_geometry
+)
+INSERT INTO tmp_countries_subcountries ("name",country_iso2,sub_country,wkb_geometry)
+SELECT "name",
+       country_iso2,
+       sub_country,
+       ST_Multi(ST_Subdivide(wkb_geometry, 255))
+FROM complex_areas_to_subdivide;
+
+DROP TABLE IF EXISTS countries_subcountries CASCADE;
+ALTER TABLE tmp_countries_subcountries RENAME TO countries_subcountries;
+ALTER INDEX tmp_countries_subcountries_wkb_geometry_geom_idx RENAME TO countries_subcountries_wkb_geometry_geom_idx;
