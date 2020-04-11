066201104d85274fd50d4443f0b9f20259c5e09b
Claas Augner <github@caugner.de>
2020-04-11 18:10:24 +0200

    Add more twitter meta tags
    Fixes #305.

--- pages/index.vue
+++ pages/index.vue
@@ -36,6 +36,10 @@ export default {
       meta: [
         { hid: 'description', name: 'description', content: description },
         { hid: 'twittercard', name: 'twitter:card', content: 'summary' },
+        { hid: 'twittersite', name: 'twitter:site', content: '@caresteouvert' },
+        { hid: 'twittertitle', name: 'twitter:title', content: this.$t('title') },
+        { hid: 'twitterdescription', name: 'twitter:description', content: description },
+        { hid: 'twitterimage', name: 'twitter:image', content: `${this.url}${this.logo}` },
         { hid: 'ogtype', property: 'og:type',  content: 'website' },
         { hid: 'ogurl', property: 'og:url',  content: this.url },
         { hid: 'ogtitle', property: 'og:title', content: this.$t('title') },
