68cf99f705f4ad34d9120c4a6947204a07307c1f
François de Metz <francois@2metz.fr>
2020-04-12 18:07:06 +0200

    Fix tests.

--- tests/details.test.js
+++ tests/details.test.js
@@ -14,14 +14,6 @@ describe('Detail', () => {
     localVue.prototype.$te = () => true;
     localVue.prototype.$t = (key) => key;
     localVue.directive('resize', {});
-
-    global.fetch = () => {
-      return Promise.resolve({
-        json() {
-          return point;
-        }
-      });
-    };
   });
 
   function createWrapper(props) {
@@ -32,51 +24,37 @@ describe('Detail', () => {
     });
   }
 
-  it('fetch the given POI', async () => {
-    point = { properties: { status: 'ouvert', cat: '', tags: {} } };
-    const detail = createWrapper({ id: '' });
-    expect(detail.vm.point).toBe(null);
-    await global.fetch();
-    await Vue.nextTick();
-    expect(detail.vm.point).not.toBe(null);
-  });
-
   it('display opening_hours', async () => {
-    point = { properties: { status: 'ouvert', cat: '', opening_hours: 'Mo-Sa 09:00-18:00', tags: {} } };
     const detail = createWrapper({ id: '' });
-    await global.fetch();
+    detail.vm.point = { properties: { status: 'ouvert', cat: '', opening_hours: 'Mo-Sa 09:00-18:00', tags: {} } };
     await Vue.nextTick();
     expect(detail.contains(DetailOpeningHours)).toBe(true);
   });
 
   it('dont display opening_hours when "open"', async () => {
-    point = { properties: { status: 'ouvert', cat: '', opening_hours: 'open', tags: {} } };
     const detail = createWrapper({ id: '' });
-    await global.fetch();
+    detail.vm.point = { properties: { status: 'ouvert', cat: '', opening_hours: 'open', tags: {} } };
     await Vue.nextTick();
     expect(detail.contains(DetailOpeningHours)).toBe(false);
   });
 
   it('display opening_hours in an alert when opening_hours present in tags', async () => {
-    point = { properties: { status: 'ouvert', cat: '', opening_hours: null, tags: { opening_hours: 'Mo-Sa 09:00-18:00' } } };
     const detail = createWrapper({ id: '' });
-    await global.fetch();
+    detail.vm.point = { properties: { status: 'ouvert', cat: '', opening_hours: null, tags: { opening_hours: 'Mo-Sa 09:00-18:00' } } };
     await Vue.nextTick();
     expect(detail.find('v-alert-stub').contains(DetailOpeningHours)).toBe(true);
   });
 
   it('display only one opening_hours', async () => {
-    point = { properties: { status: 'ouvert', cat: '', opening_hours: 'Mo-Sa 09:00-18:00', tags: { opening_hours: 'Mo-Sa 09:00-18:00' } } };
     const detail = createWrapper({ id: '' });
-    await global.fetch();
+    detail.vm.point = { properties: { status: 'ouvert', cat: '', opening_hours: 'Mo-Sa 09:00-18:00', tags: { opening_hours: 'Mo-Sa 09:00-18:00' } } };
     await Vue.nextTick();
     expect(detail.findAll(DetailOpeningHours).length).toEqual(1);
   });
 
   it('add the facebook url if not present', async () => {
-    point = { properties: { status: 'ouvert', cat: '', tags: { facebook: 'test' } } };
     const detail = createWrapper({ id: '' });
-    await global.fetch();
+    detail.vm.point = { properties: { status: 'ouvert', cat: '', tags: { facebook: 'test' } } };
     await Vue.nextTick();
     expect(detail.vm.contact('facebook')).toEqual('https://facebook.com/test');
 
@@ -85,9 +63,8 @@ describe('Detail', () => {
   });
 
   it('returns phone', async () => {
-    point = { properties: { status: 'ouvert', cat: '', tags: { phone: 'test' } } };
     const detail = createWrapper({ id: '' });
-    await global.fetch();
+    detail.vm.point = { properties: { status: 'ouvert', cat: '', tags: { phone: 'test' } } };
     await Vue.nextTick();
     expect(detail.vm.contact('phone')).toEqual('test');
   });
