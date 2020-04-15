656709824fce545523853e8ce5e1aba277952499
François de Metz <francois@2metz.fr>
2020-04-15 13:20:43 +0200

    Load the locales in nuxt.config.js to detect and reload changes.

--- modules/i18n.js
+++ modules/i18n.js
@@ -1,13 +1,7 @@
-import glob from 'glob';
-import { resolve, basename, join } from 'path';
+import { resolve, join } from 'path';
 import config from '../config.json';
 import { countries } from '../categories';
 
-const messages = {};
-glob.sync('./locales/*.json' ).forEach((file) => {
-  const locale = basename(file, '.json');
-  messages[locale] = require(resolve(file));
-});
 const domains = countries.map(c => c.toLowerCase()).map((country) => {
   return {
     domain: config[country] && config[country].domain,
@@ -15,8 +9,7 @@ const domains = countries.map(c => c.toLowerCase()).map((country) => {
   };
 }).filter(v => v.domain);
 
-
-export default function() {
+export default function({ messages }) {
   this.addPlugin({
     src: resolve('modules', 'plugin.i18n.js'),
     fileName: join('plugins', 'plugin.i18n.js')
