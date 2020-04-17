ab37d86480f4207233b2baabdddc9a80a0ac1d61
François de Metz <francois@2metz.fr>
2020-04-17 11:31:37 +0200

    Move the governement link to a component.

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -17,26 +17,7 @@
       </v-card-subtitle>
     </v-card>
 
-    <v-alert
-      v-model="alert"
-      dense
-      tile
-      dismissible
-      color="orange"
-      dark
-      class="mb-0"
-    >
-      {{ $t('covid19-alert') }}
-      <v-btn
-        icon
-        x-small
-        color="secondary"
-        :href="countryConfig.government"
-        target="_blank"
-      >
-        <v-icon>{{ `osm-info` }}</v-icon>
-      </v-btn>
-    </v-alert>
+    <governement-alert />
     <v-divider></v-divider>
     <slot />
     <v-list>
@@ -54,28 +35,21 @@
 </template>
 
 <script>
-import { getCookie, setCookie } from './cookie';
 import SidebarListItem from './sidebar_list_item';
 import ChangeLanguage from './change_language';
 import LearnMore from './learn_more';
+import GovernementAlert from './governement_alert';
 import i18nMixin from './mixins/i18n';
 
 export default {
-  components: { LearnMore, ChangeLanguage, SidebarListItem },
-
-  mixins: [i18nMixin],
-
-  data() {
-    return {
-      alert: getCookie('showAlert') === 'false' ? false : true
-    };
+  components: {
+    ChangeLanguage,
+    GovernementAlert,
+    LearnMore,
+    SidebarListItem,
   },
 
-  watch: {
-    alert() {
-      setCookie('showAlert', false);
-    }
-  }
+  mixins: [i18nMixin]
 }
 </script>
 
