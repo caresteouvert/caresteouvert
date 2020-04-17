a6284193926e37f6487553e9cf99758fbc531564
vdct <vdct@laposte.net>
2020-04-17 09:06:55 +0200

    fix shell (#189)

--- db/load_COG.sh
+++ db/load_COG.sh
@@ -2,7 +2,7 @@
 
 CONNEXION=${1}
 
-mkdir -p cog && cd cog && rm *.csv
+cd cog && rm *.csv
 
 wget -NS https://www.insee.fr/fr/statistiques/fichier/4316069/cog_ensemble_2020_csv.zip
 unzip cog_ensemble_2020_csv.zip
@@ -15,3 +15,4 @@ psql ${CONNEXION} -f create_table_cog.sql
 cat ${CSV_COMMUNE}|       psql ${CONNEXION} -c "TRUNCATE TABLE cog_commune;       COPY cog_commune FROM stdin WITH csv header delimiter ',';"
 cat ${CSV_DEPARTEMENT}|   psql ${CONNEXION} -c "TRUNCATE TABLE cog_departement;   COPY cog_departement FROM stdin WITH csv header delimiter ',';"
 cat ${CSV_REGION}|        psql ${CONNEXION} -c "TRUNCATE TABLE cog_region;        COPY cog_region FROM stdin WITH csv header delimiter ',';"
+cd -
