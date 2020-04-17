566ce58ced703bb9d234edf3c7960a00d4847666
François de Metz <francois@2metz.fr>
2020-04-17 11:31:37 +0200

    Move the governement link to a component.

---
+++ components/governement_alert.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-alert
+    v-model="alert"
+    dense
+    tile
+    dismissible
+    color="orange"
+    dark
+    class="mb-0"
+  >
+    {{ $t('covid19-alert') }}
+    <v-btn
+      icon
+      x-small
+      color="secondary"
+      :href="countryConfig.government"
+      target="_blank"
+    >
+      <v-icon>{{ `osm-info` }}</v-icon>
+    </v-btn>
+  </v-alert>
+</template>
+
+<script>
+import { getCookie, setCookie } from './cookie';
+import i18nMixin from './mixins/i18n';
+
+export default {
+  mixins: [i18nMixin],
+
+  data() {
+    return {
+      alert: getCookie('showAlert') === 'false' ? false : true
+    };
+  },
+
+  watch: {
+    alert() {
+      setCookie('showAlert', false);
+    }
+  }
+};
+</script>
