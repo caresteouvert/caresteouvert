a8d7bb689e96acc5194fc5d8150eb3fff0fb94d0
Nicolas Bétheuil <wadouk@gmail.com>
2020-04-19 11:03:43 +0200

    put it in the right place

--- components/controls/FilterControl.vue
+++ components/controls/FilterControl.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mapboxgl-ctrl mapboxgl-ctrl-group">
+  <div class="filterController mapboxgl-ctrl mapboxgl-ctrl-group">
     <button @click="$emit('deliveronly')"
             :title="$t('delivery')"
             :aria-label="$t('delivery')">
@@ -10,6 +10,23 @@
 
 <script>
 import { $helpers } from "vue-mapbox";
+
+class MyControl {
+  onAdd() {
+    this._container = document.querySelector('.filterController')
+    return this._container;
+  }
+
+  onRemove() {
+    this._container.parentNode.removeChild(this._container);
+    this._map = undefined;
+  }
+
+  getDefaultPosition() {
+    return "bottom-right"
+  }
+}
+
 export default {
   name: "FilterController",
   mixins: [$helpers.asControl],
@@ -17,9 +34,11 @@ export default {
   inject: ["mapbox", "map"],
 
   created() {
-    // this.control = this;
-    // this.position = 'bottom-right';
-    // this.$_addControl();
+    this.control = new MyControl();
+  },
+
+  mounted() {
+    this.map.addControl(this.control);
   },
 };
 </script>
