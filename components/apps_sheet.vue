a44ad2706f242b8bea3c21b6fc6f49c888b5f7c2
François de Metz <francois@2metz.fr>
2020-05-06 00:42:26 +0200

    Reformat the apps_sheet component.

--- components/apps_sheet.vue
+++ components/apps_sheet.vue
@@ -1,31 +1,59 @@
 <template>
   <div class="text-center">
-    <v-bottom-sheet v-model="show" max-width="600" inset>
+    <v-bottom-sheet
+      v-model="show"
+      max-width="600"
+      inset
+    >
       <v-sheet class="text-center">
         <div class="pt-3 pb-0 mb-0 title">{{ $t('apps_info.text', { brand }) }}</div>
-          <v-container class="pb-0">
-            <v-row no-gutters align="center" justify="space-around">
-              <v-col cols="6" sm="4">
-                <a href="https://play.google.com/store/apps/details?id=com.transway.caresteouvert" target="_blank" rel="noopener">
-                  <img src="../assets/appbadge_gplay.png" class="img-badge" :alt="$t('apps_info.googleplay')" />
-                </a>
-              </v-col>
-              <v-col cols="6" sm="4">
-                <a href="https://apps.apple.com/fr/app/%C3%A7a-reste-ouvert/id1506199151" target="_blank" rel="noopener">
-                  <img src="../assets/appbadge_apple.png" class="img-badge" :alt="$t('apps_info.apple')" />
-                </a>
-              </v-col>
-              <v-col cols="6" sm="4">
-                <a href="https://f-droid.org/fr/packages/com.transway.caresteouvert/" target="_blank" rel="noopener">
-                  <img src="../assets/appbadge_fdroid.png" class="img-badge" :alt="$t('apps_info.fdroid')" />
-                </a>
-              </v-col>
-            </v-row>
-          </v-container>
+        <v-container class="pb-0">
+          <v-row no-gutters align="center" justify="space-around">
+            <v-col cols="6" sm="4">
+              <a
+                href="https://play.google.com/store/apps/details?id=com.transway.caresteouvert"
+                target="_blank"
+                rel="noopener"
+              >
+                <img
+                  :alt="$t('apps_info.googleplay')"
+                  src="../assets/appbadge_gplay.png"
+                  class="img-badge"
+                />
+              </a>
+            </v-col>
+            <v-col cols="6" sm="4">
+              <a
+                href="https://apps.apple.com/fr/app/%C3%A7a-reste-ouvert/id1506199151"
+                target="_blank"
+                rel="noopener"
+              >
+                <img
+                  :alt="$t('apps_info.apple')"
+                  src="../assets/appbadge_apple.png"
+                  class="img-badge"
+                />
+              </a>
+            </v-col>
+            <v-col cols="6" sm="4">
+              <a
+                href="https://f-droid.org/fr/packages/com.transway.caresteouvert/"
+                target="_blank"
+                rel="noopener"
+              >
+                <img
+                  :alt="$t('apps_info.fdroid')"
+                  src="../assets/appbadge_fdroid.png"
+                  class="img-badge"
+                />
+              </a>
+            </v-col>
+          </v-row>
+        </v-container>
         <v-btn
           text
-          @click="seen"
           class="mb-3"
+          @click="seen"
         >{{ $t('apps_info.close') }}</v-btn>
       </v-sheet>
     </v-bottom-sheet>
@@ -55,10 +83,9 @@ export default {
   },
 
   watch: {
-    show: {
-      immediate: false,
-      handler(value) {
-        if (!value) { this.seen(); }
+    show(value) {
+      if (!value) {
+        this.seen();
       }
     }
   },
