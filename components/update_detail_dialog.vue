0523ad03cefbbb7394855134795d3380926e10ce
PanierAvide <panieravide@riseup.net>
2020-05-07 08:22:24 +0200

    Add strings from complete POI dialog in locales files #683

--- components/update_detail_dialog.vue
+++ components/update_detail_dialog.vue
@@ -10,7 +10,7 @@
             <v-icon>osm-alert</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>C'est votre établissement ? Complétez vos informations</v-list-item-title>
+            <v-list-item-title>{{ $t('details.update_dialog.complete') }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -18,28 +18,24 @@
 
     <v-card>
       <v-card-title class="flex-nowrap">
-        <span class="body-1">Compléter les informations de votre établissement</span>
+        <span class="body-1">{{ $t('details.update_dialog.title') }}</span>
         <v-spacer></v-spacer>
         <v-btn
           icon
-          @click="dialog = false"
+          @click="dialog=false"
         >
           <v-icon>osm-close</v-icon>
         </v-btn>
       </v-card-title>
       <v-card-text>
-        <p>
-          Pour rajouter des informations vous pouvez
-          <a href="https://blog.caresteouvert.fr/completer-les-infos-dun-lieu/"
-            target="_blank">éditer vous-même votre établissement sur OpenStreetMap</a>.
-        </p>
+        <p v-html="$t('details.update_dialog.use_osm')"></p>
         <br />
         <p class="text-center">
           <v-btn
             :href="`https://airtable.com/shr9dsYsPX0VvUoUY?prefill_osm_id=${osm}&prefill_osm_name=${place.properties.name}`"
             target="_blank"
             color="primary"
-          >Besoin d'aide ? Contactez nous</v-btn>
+          >{{ $t('details.update_dialog.contact') }}</v-btn>
         </p>
       </v-card-text>
     </v-card>
